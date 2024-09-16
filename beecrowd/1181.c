185ed51af156c151270b8ada295dc0ff84bd8ea7
Vinicius Victor <viniciusvictor443@gmail.com>
2024-09-16 18:22:14 -0300

    solucoes

--- beecrowd/1181.c
+++ beecrowd/1181.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 
 int main(){
-    int i, L, M[12], soma = 0;
-    float media;
+    float M[12][12];
+    int linha;
     char op;
+    float soma = 0, media;
 
-    scanf("%d%*c", &L);
+    scanf("%d%*c", &linha);
     scanf("%c", &op);
 
-    if (op == 'S'){
-        for (i = 0; i < 12; i++){
-            scanf("%d", &M[i]);
-            soma += M[i];
+    for (int i = 0; i <= 11; i++){
+        for (int j = 0; j <= 11; j++){
+            scanf("%f", &M[i][j]);
         }
-
-        printf("%d\n", soma);
-    }else{
-        for (i = 0; i < 12; i++){
-            scanf("%d", &M[i]);
-            media += (float)M[i]/12;
-        }
-
-        printf("%.1f\n", media);
     }
 
 
+    for (int j = 0; j <= 11; j++){
+        scanf("%f", M[linha][j]);
+        soma += M[linha][j];
+    }
 
-    
+    if (op == 'S')
+        printf("%.1f\n", soma);
+    else
+        printf("%.1f\n", soma / 12);
 
     return 0;
 }
